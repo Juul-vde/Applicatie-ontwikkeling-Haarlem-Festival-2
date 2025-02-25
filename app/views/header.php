d2d0e6bff284672a708bc946d801aaa1b1fcf1a8
Daniel <danielflierman3@gmail.com>
2025-02-25 17:25:17 +0100

    Basic nav bar design

--- app/views/header.php
+++ app/views/header.php
@@ -2,8 +2,35 @@
 <html lang="en">
 
 <head>
-
+    <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 
 <body>
+    <header class="p-3 text-white">
+        <div class="container">
+            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                <img src="/media/logoplaceholder.png" alt="Logo" width="40" height="40" class="me-2">
+            </a>
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-2 text-white">HOME</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">DANCE!</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">FOOD</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">HISTORY</a></li>
+            </ul>
+
+            <div class="text-end">
+                <button type="button" class="btn btn-outline-light me-2">Login</button>
+                <button type="button" class="btn btn-signup">Sign-up</button>
+            </div>
+        </div>
+    </header>
     <main>
+        
+    </main>
+</body>
+
+</html>
